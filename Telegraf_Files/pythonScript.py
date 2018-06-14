a661c30e808dd3be10d199fa7324490640a6d432
Danny Franken <danny@frankenprojects.com>
2018-06-15 00:07:50 +0200

    Modified pyhton script

--- Telegraf_Files/pythonScript.py
+++ Telegraf_Files/pythonScript.py
@@ -9,8 +9,6 @@ from io import BytesIO
 TCP_IP = '127.0.0.1'
 TCP_PORT = 8080
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socketConnected = False
-
 
 
 
@@ -31,11 +29,12 @@ def send_to_telegraf(content):
 		connect_socket()
 
 #Setup TCP-Socket
-def connect_socket(self):
-	while not self.socketConnected:
+def connect_socket():
+	connected = False
+	while not connected:
 		try:	
-			self.socket.connect((TCP_IP, TCP_PORT))
-			self.socketConnected = True
+			socket.connect((TCP_IP, TCP_PORT))
+			connected = True
 		except:
 			print("Failed to connect to Telegraf")
 			time.sleep(1)
